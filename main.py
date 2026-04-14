6c8649ea0d0acf9b6364e29998075b25576c50e3
AlejandroRivs <Alejandro12896@gmail.com>
2026-04-14 11:16:14 -0600

    inicio y prueba del proyecto

---
+++ main.py
@@ -0,0 +1,22 @@
+class Pokemon:
+    def __init__(self, nombre, hp_actual, hp_maximo, energia_actual, energia_maxima, tipo):
+        self.nombre = nombre
+        self.tipo = tipo
+        self.hp_actual = hp_actual
+        self.hp_maximo = hp_maximo
+        self.energia_actual = energia_actual
+        self.energia_maxima = energia_maxima
+    @property
+    def hp_actual(self):
+        return self.hp_actual
+    def energia_actual(self):
+        return self.energia_actual
+    @hp_actual.setter
+    def hp_actual(self):
+        pass
+    @energia_actual.setter
+    def energia_actual(self):
+        pass
+
+        
+    
