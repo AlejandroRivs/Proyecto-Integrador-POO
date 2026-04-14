0bcb3915921faa48989bb5d30554e319e5d45327
AlejandroRivs <Alejandro12896@gmail.com>
2026-04-14 11:52:39 -0600

    avance en la estructura visual del proyecto

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pokedex import mostrar_catalogo_disponible
 class Pokemon:
     def __init__(self, nombre, hp_actual, hp_maximo, energia_actual, energia_maxima, tipo):
         self.nombre = nombre
@@ -6,9 +7,11 @@ class Pokemon:
         self.hp_maximo = hp_maximo
         self.energia_actual = energia_actual
         self.energia_maxima = energia_maxima
+   
     @property
     def hp_actual(self):
         return self.hp_actual
+    @property
     def energia_actual(self):
         return self.energia_actual
     @hp_actual.setter
@@ -17,3 +20,12 @@ class Pokemon:
     @energia_actual.setter
     def energia_actual(self):
         pass
+
+
+print(f"\n{'╔' + '='*100 + '╗'}\n║{">>> SIMULADOR DE BATALLAS POKEMON <<<":^100}║\n{'╚' + '='*100 + '╝'}")
+print(f"\n\t{'SELECCIONE MODO DE JUEGO:':<100}\n\t[1] Jugador vs Jugador\n\t[2] Jugador vs Computadora\n{'='*102}")
+opcion = input(f"\tIngrese modo de juego (1 o 2): \t")
+#mostrar_catalogo_disponible()
+
+
+print
