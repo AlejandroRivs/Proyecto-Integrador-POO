330b2da34975a8e427dac3dcfdba09da47049006
AlejandroRivs <Alejandro12896@gmail.com>
2026-04-15 11:28:46 -0600

    creacion de pokemon_clases.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from pokedex import mostrar_catalogo_disponible
-class Pokemon:
+from abc import ABC, abstractmethod
+#crear la base abstracta "pokemon"
+class Pokemon(ABC):
     def __init__(self, nombre, hp_actual, hp_maximo, energia_actual, energia_maxima, tipo):
         self.nombre = nombre
         self.tipo = tipo
@@ -7,7 +9,11 @@ class Pokemon:
         self.hp_maximo = hp_maximo
         self.energia_actual = energia_actual
         self.energia_maxima = energia_maxima
-   
+    @abstractmethod
+    def atacar(self, pokemon_enemigo):
+        pass
+#crear 4 clases hijas en otro archivo
+
     @property
     def hp_actual(self):
         return self.hp_actual
@@ -21,11 +27,20 @@ class Pokemon:
     def energia_actual(self):
         pass
 
-
 print(f"\n{'╔' + '='*100 + '╗'}\n║{">>> SIMULADOR DE BATALLAS POKEMON <<<":^100}║\n{'╚' + '='*100 + '╝'}")
+print(f"{'Escriba salir para cerrar el juego.':>100}")
 print(f"\n\t{'SELECCIONE MODO DE JUEGO:':<100}\n\t[1] Jugador vs Jugador\n\t[2] Jugador vs Computadora\n{'='*102}")
-opcion = input(f"\tIngrese modo de juego (1 o 2): \t")
+
+while True:
+    opcion = input(f"\tIngrese modo de juego (1 o 2): \t")
+    if opcion == "1":
+        pass
+    elif opcion == "2":
+        pass
+    elif opcion.lower == "salir":
+        break
+    else:
+        print("")
+
 #mostrar_catalogo_disponible()
 
-
-print
---
+++ pokemon_clases.py
@@ -0,0 +1,14 @@
+#crear 4 clases importando la clase "Pokemon"  (desde main) como padre
+from main import Pokemon
+class PokemonAgua(Pokemon):
+    def atacar(self, pokemon_enemigo):
+        pass
+class PokemonFuego(Pokemon):
+    def atacar(self, pokemon_enemigo):
+        pass
+class PokemonPlanta(Pokemon):
+    def atacar(self, pokemon_enemigo):
+        pass
+class PokemonElectrico(Pokemon):
+    def atacar(self, pokemon_enemigo):
+        pass
