e0d56bbbaea63e89f18e338ef0ee3b2bb098dac8
AlejandroRivs <alejandro12896@gmail.com>
2026-04-22 05:08:00 -0600

    fin de la logica de batalla, se muestra el ganador arreglo de multiples errores en codigo

--- main.py
+++ main.py
@@ -2,63 +2,73 @@ from pokedex import mostrar_catalogo_disponible, CATALOGO_POKEMON
 from abc import ABC, abstractmethod
 from random import randint
 #uso de * para importar todo ya que en ese archivo solo existen esas 4 clases
-from pokemon_clases import *
-
 
 #crear la base abstracta "pokemon"
 class Pokemon(ABC):
-    def __init__(self, nombre, tipo, hp_actual, hp_maximo, energia_actual, energia_maxima):
+    def __init__(self, nombre, hp_maximo, energia_maxima):
         self.nombre = nombre
-        self.tipo = tipo
         #protegemos estos atributos con _
-        self._hp_actual = hp_actual
-        self._hp_maximo = hp_maximo
-        self._energia_actual = energia_actual
-        self._energia_maxima = energia_maxima
+        self.__hp_maximo = hp_maximo
+        self.__hp_actual = hp_maximo
+        self.__energia_maxima = energia_maxima
+        self.__energia_actual = energia_maxima
+        self.paralizado = False  # Atributo para indicar si el Pokémon está paralizado
+        self.defendiendo = False  # Atributo para indicar si el Pokémon está defendiendo
+
     #metodo abstracto para que cada clase hija tenga su propia implementacion de ataque
     @abstractmethod
     def atacar(self, pokemon_enemigo):
         pass
-    
     #crear 4 clases hijas en pokemon_clases.py
 
     @property
+    def hp_maximo(self):
+        return self.__hp_maximo #atributoprivado
+    
+    @property
     def hp_actual(self):
-        return self._hp_actual  #atributoprivado
+        return self.__hp_actual  #atributoprivado
     
     @hp_actual.setter
     def hp_actual(self, valor):
         # Validación, no bajar de 0 ni subir del maximo
         if valor < 0:
-            self._hp_actual = 0
-        elif valor > self._hp_maximo:
-            self._hp_actual = self._hp_maximo
+            self.__hp_actual = 0
+        elif valor > self.__hp_maximo:
+            self.__hp_actual = self.__hp_maximo
         else:
-            self._hp_actual = valor
+            self.__hp_actual = valor
     
     @property
     def energia_actual(self):
-        return self._energia_actual  #atributoprivado 
-        # Validación, no bajar de 0 ni subir del maximo
+        return self.__energia_actual  #atributoprivado 
 
     @energia_actual.setter
     def energia_actual(self, valor):
         # Validación, no bajar de 0 ni subir del maximo
         if valor < 0:
-            self._energia_actual = 0
-        elif valor > self._energia_maxima:
-            self._energia_actual = self._energia_maxima
+            self.__energia_actual = 0
+        elif valor > self.__energia_maxima:
+            self.__energia_actual = self.__energia_maxima
         else:
-            self._energia_actual = valor
+            self.__energia_actual = valor
+    
+    @property
+    def energia_maxima(self):
+        return self.__energia_maxima #atributoprivado
+
 
 class Jugador:
     def __init__(self, numero_jugador, pokemon_seleccionado=None):
         self.numero_jugador = numero_jugador
         self.pokemon_elegido = pokemon_seleccionado
 
-
     def seleccionar_pokemon(self):
+        #importacion local para evitar problemas de importacion |
+        from pokemon_clases import PokemonAgua, PokemonFuego, PokemonPlanta, PokemonElectrico
+
         while True:
+            #jugador 3 indica que es la pc
             if self.numero_jugador == 3:
                 opcion_catalogo = str(randint(1, len(CATALOGO_POKEMON)))
             else:
@@ -74,7 +84,7 @@ class Jugador:
                     #usamos el tipo como llave para obtener la clase
                     clase_pokemon = clases_por_tipo[tipo]
                     #reducimos los if a una sola linea usando la clase obtenida del diccionario
-                    self.pokemon_elegido = clase_pokemon(datos["nombre"], datos["tipo"], datos["hp_maximo"], datos["hp_maximo"], datos["energia_maxima"], datos['energia_maxima'])
+                    self.pokemon_elegido = clase_pokemon(datos["nombre"], datos["hp_maximo"], datos["energia_maxima"])
                     print(f"\n\t\tJugador {self.numero_jugador}, ha seleccionado a {datos['nombre']} como su pokemon.")
                     return self.pokemon_elegido
                 
@@ -83,35 +93,119 @@ class Jugador:
             if opcion_catalogo == "salir":
                 break
     
-    def atacar(self, pokemon_enemigo, multiplicador):
-        self.pokemon_elegido.energia_actual -= 10
-        pokemon_enemigo.hp_actual -= (10 * multiplicador)
-        print(f"{self.pokemon_elegido.nombre} ha atacado a {pokemon_enemigo.nombre} con un ataque normal. {pokemon_enemigo.nombre} pierde 10 HP y {self.pokemon_elegido.nombre} pierde 10 EP.")
+class SimuladorPokemon:
+    def __init__(self):
+        self.jugador1 = Jugador(1)
+        self.jugador2 = Jugador(2)
+        self.jugador_pc = Jugador(3)
+        self.opcion = ""
 
-jugador1=Jugador(1)
-jugador2=Jugador(2)
-jugador_pc=Jugador(3)
+    #creamos un metodo para mostrar el catalogo ordenado ya que el metodo de pokedex.py no se adapta a nuestro formato
+    @staticmethod
+    
+    def mostrar_catalogo_ordenado():
+        print(f"\n{' POKÉDEX DISPONIBLE ':=^{102}}")
+        
+        # Usamos el for sobre las llaves del diccionario
+        for clave in CATALOGO_POKEMON:
+            datos = CATALOGO_POKEMON[clave]
+            print(f"{f'[{clave}]':<3} | {datos['nombre']:<15} | {f'Tipo: {datos['tipo']}':<15} | {f'HP: {datos['hp_maximo']}':<10} | {f'EP: {datos['energia_maxima']}':<10}".center(102))        
+            
+        print("=" * 102)
+    @staticmethod
+    
+    def mostrar_encabezado():
+        print(f"\n{'╔' + '='*100 + '╗'}\n║{">>> SIMULADOR DE BATALLAS POKEMON <<<":^100}║\n{'╚' + '='*100 + '╝'}")
+        print(f"{'Escriba salir para cerrar el juego.':>100}")
+        print(f"\n\t{'SELECCIONE MODO DE JUEGO:':<100}\n\t[1] Jugador vs Jugador\n\t[2] Jugador vs Computadora\n")
+    
+    def iniciar_batalla(self, jugador1, jugador2):
+        print(f"\n{'╔' + '='*100 + '╗'}\n║{">>> ¡COMIENZA LA BATALLA! <<<":^100}║\n{'╚' + '='*100 + '╝'}")
+        print(f"{'Escriba salir si desea abandonar el combate.':>100}")
+        combate = f"{jugador1.pokemon_elegido.nombre} vs {jugador2.pokemon_elegido.nombre}"
+        print(f"{combate:^100}\n")
+        costo_ataque = 15
+        costo_defensa = 5
+        aumento_descanso = 20
 
-print(f"\n{'╔' + '='*100 + '╗'}\n║{">>> SIMULADOR DE BATALLAS POKEMON <<<":^100}║\n{'╚' + '='*100 + '╝'}")
-print(f"{'Escriba salir para cerrar el juego.':>100}")
-print(f"\n\t{'SELECCIONE MODO DE JUEGO:':<100}\n\t[1] Jugador vs Jugador\n\t[2] Jugador vs Computadora\n{'='*102}")
+        while jugador1.pokemon_elegido.hp_actual > 0 and jugador2.pokemon_elegido.hp_actual > 0:    
+            for jugador in [jugador1, jugador2]:
+                print(f"Turno del jugador {jugador.numero_jugador}: {jugador.pokemon_elegido.nombre} \n(HP: {jugador.pokemon_elegido.hp_actual}/{jugador.pokemon_elegido.hp_maximo}, EP: {jugador.pokemon_elegido.energia_actual}/{jugador.pokemon_elegido.energia_maxima})\n")
+                
+                #un if para determinar quien es el jugador actual y quien el oponente
+                oponente = jugador2 if jugador == jugador1 else jugador1
 
-while True:
-    opcion = input(f"\tIngrese modo de juego (1 o 2): \t")
-    if opcion == "1":
-        mostrar_catalogo_disponible()
-        jugador1.seleccionar_pokemon()
-        jugador2.seleccionar_pokemon()
-        break
-    elif opcion == "2":
-        mostrar_catalogo_disponible()
-        jugador1.seleccionar_pokemon()
-        jugador_pc.seleccionar_pokemon()
-        break
-    elif opcion == "salir":
-        break
-    else:
-        print("")
+                #validacion de parálisis y defensa antes del input para evitar inputs innecesarios
+                if jugador.pokemon_elegido.paralizado:
+                    print(f"\n{jugador.pokemon_elegido.nombre} está paralizado y pierde su turno.")
+                    jugador.pokemon_elegido.paralizado = False  # Se quita la parálisis después de perder un turno
+                    continue  # Salta el turno del jugador paralizado
 
-#mostrar_catalogo_disponible()
+                if jugador.pokemon_elegido.defendiendo:
+                    print(f"\n{jugador.pokemon_elegido.nombre} está defendiendo y recibirá menos daño en este turno.")
+                    jugador.pokemon_elegido.defendiendo = False  # Se quita el modo defensa después de un turnol     
+                
+                #en caso que el jugador sea la computadora(jugador 3)
+                if jugador.numero_jugador == 3:
+                    opcion_batalla = randint(1, 3)  # La computadora elige aleatoriamente entre atacar, defender o descansar
+                    print(f"La computadora elige la opcion {opcion_batalla}")
+                else:
+                    print(f"[1] Atacar (costo: {costo_ataque} EP) \n[2] Defender (costo: {costo_defensa} EP)\n[3] Descansar (restaura: {aumento_descanso} EP)\n")
+                    try:
+                        opcion_batalla = int(input("\nIngrese el número de la acción que desea realizar: "))      
+                    except ValueError:
+                        print("Por favor, ingrese un número válido.")              
+                #atacar
+                if opcion_batalla == 1: 
+                    if jugador.pokemon_elegido.energia_actual >= costo_ataque:
+                        daño = jugador.pokemon_elegido.atacar(oponente.pokemon_elegido)
+                        print(f"\n{jugador.pokemon_elegido.nombre} ha atacado a {oponente.pokemon_elegido.nombre} causando {daño} puntos de daño.")
+                        jugador.pokemon_elegido.energia_actual -= costo_ataque
+                    else:
+                        print(f"\nNo tienes suficiente energía para atacar. Necesitas al menos {costo_ataque} EP.")
+                #defender
+                elif opcion_batalla == 2:
+                    if jugador.pokemon_elegido.energia_actual >= costo_defensa:
+                        jugador.pokemon_elegido.defendiendo = True
+                        jugador.pokemon_elegido.energia_actual -= costo_defensa
+                        print(f"\n{jugador.pokemon_elegido.nombre} esta en modo defensa, recibiraa menos daño en el siguiente turno.")
+                    else:
+                        print(f"\nNo tienes suficiente energía para defender. Necesitas al menos {costo_defensa} EP.")
 
+                #descansar
+                elif opcion_batalla == 3:
+                    jugador.pokemon_elegido.energia_actual += aumento_descanso
+                    print(f"\n{jugador.pokemon_elegido.nombre} ha descansado y recupera {aumento_descanso} EP.")
+
+                #verificar hp luego de cada accion para evitar acciones ilogicas
+                if oponente.pokemon_elegido.hp_actual <= 0:
+                    print(f"\n{oponente.pokemon_elegido.nombre} se ha debilitado.")
+                    print(f"\n{'╔' + '='*100 + '╗'}\n║{f'¡El jugador {jugador.numero_jugador} ha ganado la batalla con {jugador.pokemon_elegido.nombre}!':^100}║\n{'╚' + '='*100 + '╝'}")
+                    return
+            
+        
+    def menu(self):
+        self.mostrar_encabezado()     
+        while True:
+            opcion = input(f"\tIngrese modo de juego (1 o 2): \t")    
+
+            if opcion == "1":
+                self.mostrar_catalogo_ordenado()
+                self.jugador1.seleccionar_pokemon()
+                self.jugador2.seleccionar_pokemon()
+                self.iniciar_batalla(self.jugador1, self.jugador2)
+            elif opcion == "2":
+                self.mostrar_catalogo_ordenado()
+                self.jugador1.seleccionar_pokemon()
+                self.jugador_pc.seleccionar_pokemon()
+                self.iniciar_batalla(self.jugador1, self.jugador_pc)
+            elif opcion == "salir":
+                print("¡Gracias por jugar! ¡Hasta la próxima!")
+                break
+            else:
+                print("Opción no válida. Por favor, ingrese 1, 2 o 'salir'.")
+            
+
+if __name__ == "__main__":
+    simulador = SimuladorPokemon()
+    simulador.menu()
--- pokemon_clases.py
+++ pokemon_clases.py
@@ -1,30 +1,50 @@
 #crear 4 clases importando la clase "Pokemon"  (desde main) como padre
+from random import randint
 daño = 10
-multiplicador = 1
 from main import Pokemon
 class PokemonAgua(Pokemon):
-    def __init__(self, nombre, hp_actual, hp_maximo, energia_actual, energia_maxima, tipo):
-        super().__init__(self, nombre, hp_actual, hp_maximo, energia_actual, energia_maxima, tipo)
+    def __init__(self, nombre, hp_maximo,energia_maxima):
+        super().__init__(nombre, hp_maximo, energia_maxima)
         
     def atacar(self, pokemon_enemigo):
+        multiplicador = 1
         if isinstance(pokemon_enemigo, PokemonFuego):
             multiplicador = 2
         daño_final = daño * multiplicador
         pokemon_enemigo.hp_actual -= daño_final
+        return daño_final
 
 class PokemonFuego(Pokemon):
+    def __init__(self, nombre, hp_maximo,energia_maxima):
+        super().__init__(nombre, hp_maximo, energia_maxima)
     def atacar(self, pokemon_enemigo):
+        multiplicador = 1
         if isinstance(pokemon_enemigo, PokemonPlanta):
             multiplicador = 2
         daño_final = daño * multiplicador
         pokemon_enemigo.hp_actual -= daño_final
+        return daño_final
 
 class PokemonPlanta(Pokemon):
+    def __init__(self, nombre, hp_maximo,energia_maxima):
+        super().__init__(nombre, hp_maximo, energia_maxima)
     def atacar(self, pokemon_enemigo):
+        multiplicador = 1
         if isinstance(pokemon_enemigo, PokemonAgua):
             multiplicador = 2
         daño_final = daño * multiplicador
         pokemon_enemigo.hp_actual -= daño_final
+        return daño_final
 
 class PokemonElectrico(Pokemon):
-    pass
+    def __init__(self, nombre, hp_maximo,energia_maxima):
+        super().__init__(nombre, hp_maximo, energia_maxima)
+    def atacar(self, pokemon_enemigo):
+        multiplicador = 1
+        daño_final = daño * multiplicador
+        pokemon_enemigo.hp_actual -= daño_final
+        paralizar = randint(1, 5)  # 20% de probabilidad de paralizar
+        if paralizar == 1:
+            print(f"{pokemon_enemigo.nombre} ha sido paralizado y no podrá atacar en el próximo turno.")
+            pokemon_enemigo.paralizado = True
+        return daño_final
